7f76093943b3032065969c5c6975abd58d8a4965
TetyKIller <jesusalexisaliceagonzalez@gmail.com>
2021-09-10 01:23:29 -0700

    NO sirvio

--- funciones.php
+++
@@ -1,9 +0,0 @@
-<?php
-function dbDelete ($id){
-        $idpagos=$id;
-		require_once 'conexion.php';
-        $conn = dbConnect();      
-$sql = "DELETE FROM usuarios WHERE id=".$idpagos;
-mysql_query($sql, $conn);
-}
-?>
