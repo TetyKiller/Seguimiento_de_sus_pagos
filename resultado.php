a6c908f854b67157192dd193fa34881d8278af1b
TetyKIller <jesusalexisaliceagonzalez@gmail.com>
2021-09-10 03:56:17 -0700

    Implementacion de session

--- resultado.php
+++ resultado.php
@@ -1,5 +1,7 @@
 <?php
+ session_start();
+ $id = $_SESSION['ide']; 
     require_once 'conexion.php';
     $conn = dbConnect();
-	$result=mysqli_query($conn,"SELECT * from pagos");
+	$result=mysqli_query($conn,"SELECT * from pagos WHERE usuarios_id= '$id'");
 ?>
