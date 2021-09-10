956f0db4b10a7c6ca7d912674f402bbf0a521e30
TetyKIller <jesusalexisaliceagonzalez@gmail.com>
2021-09-10 03:54:58 -0700

    Correcion de redireccion

--- validacion_login.php
+++ validacion_login.php
@@ -1,11 +1,12 @@
 <?php
-
 $nombre = $_POST['nombre'];
 $password = $_POST['contrasena'];
-
+$id="";
 		require_once 'conexion.php';
   $conn = dbConnect();
  
+
+
   $consulta = mysqli_query ($conn, "SELECT * FROM usuarios WHERE name = '$nombre' AND password = '$password'");  
 
 
@@ -13,8 +14,15 @@ $password = $_POST['contrasena'];
       echo mysqli_error($mysqli);
       exit;
   } 
+  
   if($nombre = mysqli_fetch_assoc($consulta)) {
       $id = $nombre["id_user"];
-    } else{ 
-    }
+      session_start();
+     $_SESSION['ide'] = $id;
+      header("Location: home.php");
+
+  } else {
+    header("Location: index.php");
+
+  }
 ?>
