50c7dbc82ffb311593063b8f9e55df35253802a6
TetyKIller <jesusalexisaliceagonzalez@gmail.com>
2021-09-10 03:56:50 -0700

    correcion

--- conexion.php
+++ conexion.php
@@ -10,5 +10,19 @@ function dbConnect (){
 	}
 	return $conn;
  }
-
+ function dbid (){
+	$nombre = $_POST['nombre'];
+	$password = $_POST['contrasena'];
+	$id="";
+	
+			require_once 'conexion.php';
+	  $conn = dbConnect();
+	 
+	  $consulta = mysqli_query ($conn, "SELECT * FROM usuarios WHERE name = '$nombre' AND password = '$password'");  
+	
+	  if($nombre = mysqli_fetch_assoc($consulta)) {
+		  $id = $nombre["id_user"];
+	  }
+	  return $id;
+ }
  ?>
