323857563bef6fadc8e26f98d6be45306ef2d282
TetyKIller <jesusalexisaliceagonzalez@gmail.com>
2021-09-09 22:46:51 -0700

    Creo el login con diseno basico

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="estilos_index.css">
+</head>
+    <body>
+        <form class="form-inse" method="post" action="" name="form-inse">
+                <label>Usuario: </label>
+                <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+                <label>Contrasena: </label>
+                <input type="contrasena" name="contrasena" required />
+            <button type="submit" name="inse" value="inse">Iniciar sesión</button>
+        </form>
+    </body>
+</html>
