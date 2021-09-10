0515a9fbc596a92f6e07d68fbdbda0f8705457e8
TetyKIller <jesusalexisaliceagonzalez@gmail.com>
2021-09-10 03:55:59 -0700

    Correciones

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-    include ("validacion_login.php")
+s
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,23 +13,26 @@
     <body>
         <header>
         <h1>Pagos Realizados</h1>
+        <form action="insertar.php" method="post">
+                        <button type="submit" name="btningresar"  >Ingresar pago</button>
+        </form>
         </header>
         <main>
             <?php
-                	include("resultado.php");
+                include("resultado.php");
                 while($mostrar=mysqli_fetch_array($result)){
             ?>
                 <div class="res">
-                <p><?php echo $mostrar['id_pagos'] ?></p>
-                <p><?php echo $mostrar['nombre'] ?></p>
-                <p><?php echo $mostrar['fecha'] ?></p>
-                <p><?php echo $mostrar['catidad'] ?></p>
+                    <p><?php echo $mostrar['id_pagos'] ?></p>
+                    <p><?php echo $mostrar['nombre'] ?></p>
+                    <p><?php echo $mostrar['fecha'] ?></p>
+                    <p><?php echo $mostrar['catidad'] ?></p>
                     <form action="borrar.php" method="post">
-                    <button type="submit" name="dato" value=<?php echo $mostrar['id_pagos'] ?>>Borrar</button>
+                        <button type="submit" name="dato" value=<?php echo $mostrar['id_pagos'] ?>>Borrar</button>
                     </form>
                 </div>
                 <?php
-                }
+                     }
                 ?>
         </main>
     </body>
